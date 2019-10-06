71abc37e256ca47a2c92fb0ff3e1d926e51f02d2
Colby Ackerman <csaz86@umsystem.edu>
2019-10-05 21:03:20 -0500

    added keys for semaphore, msg, clock

--- sharedMemoryKeys.h
+++ sharedMemoryKeys.h
@@ -1,6 +1,9 @@
 #ifndef SHARED_MEMORY_KEYS_H
 #define SHARED_MEMORY_KEYS_H
 
-#define MSG_KEY 1337
+//#define MSG_KEY 1337
+#define MSG_KEY 0x13333337
+#define SEM_KEY 0x01337357
+#define CLOCK_KEY 0x00031337
 
 #endif
